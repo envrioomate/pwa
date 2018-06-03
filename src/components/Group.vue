f9723a42ad0f37d5501959d78e7b81722c2fa227
Timo Volk <timo.volk@gmail.com>
2018-06-03 15:01:43 +0200

    group view now shows the actual data

--- src/components/Group.vue
+++ src/components/Group.vue
@@ -9,13 +9,13 @@
 					<v-list two-line v-if='hasGroup'>
 					  <template v-for="(item, index) in group.members">
 						<v-subheader v-if="item.header" :key="item.header">{{ item.header }}</v-subheader>
-						<v-divider v-else-if="item.divider" :inset="item.inset" :key="index"></v-divider>
-						<v-list-tile v-else :key="item.title" avatar @click="">
-						  <v-list-tile-avatar :color="item.color">
-							<span class="white--text headline">{{ item.avatar }}</span>
+						<v-divider v-else-if="false" :inset="false" :key="index"></v-divider>
+						<v-list-tile v-else :key="item.screenName" avatar @click="">
+						  <v-list-tile-avatar :color="colors">
+							<span class="white--text headline">{{ item.screenName.charAt(0) }}</span>
 						  </v-list-tile-avatar>
 						  <v-list-tile-content>
-							<v-list-tile-title v-html="item.title"></v-list-tile-title>
+							<v-list-tile-title v-html="item.screenName"> {{item.screenName}} Hello</v-list-tile-title>
 							<v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
 						  </v-list-tile-content>
 						</v-list-tile>
@@ -40,9 +40,17 @@
             avatars: function () {
                 return group.members.map((member) => member.screenName.charAt(0))
             },
+            colors: function () {
+                var colors= [
+                    'red', 'pink', 'purple', 'blue', 'cyan', 'teal', 'yellow', 'red'
+
+                ];
+                var randomNumber = Math.floor(Math.random()*colors.length);
+                return colors[randomNumber];
+            },
             ...mapGetters({
                 hasGroup: 'hasGroup',
-                group: 'group',
+                group: 'myGroup',
                 inviiteId: 'inviteId',
                 token: 'token'
 
