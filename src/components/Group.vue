893d06fafacad25a827860758abd914545b56467
Timey16 <timey16@googlemail.com>
2018-06-18 12:53:36 +0200

    changed tree graphics to transparent
    and added them to the score board
    
    also changed score to be an icon

--- src/components/Group.vue
+++ src/components/Group.vue
@@ -26,7 +26,17 @@
                                             </v-list-tile>
                                         </template>
                                     </v-list>
-                                    <h3 class="display-2">Score: {{group ? group.score : "placeholder"}}</h3>
+                                    <div><h3 class="display-2"><v-icon x-large>nature_people</v-icon>: {{group ? group.score : "placeholder"}}</h3></div>
+									<div>
+										<img src="../assets/Baum/8.png" v-if="group.score > 7000" height="200px"></img>
+										<img src="../assets/Baum/7.png" v-else-if="group.score > 6000" height="200px"></img>
+										<img src="../assets/Baum/6.png" v-else-if="group.score > 5000" height="200px"></img>
+										<img src="../assets/Baum/5.png" v-else-if="group.score > 4000" height="200px"></img>
+										<img src="../assets/Baum/4.png" v-else-if="group.score > 3000" height="200px"></img>
+										<img src="../assets/Baum/3.png" v-else-if="group.score > 2000" height="200px"></img>
+										<img src="../assets/Baum/2.png" v-else-if="group.score > 1000" height="200px"></img>
+										<img src="../assets/Baum/1.png" v-else height="200px"></img>
+									</div>
                                 </div>
                                 <v-fab-transition>
                                     <v-btn
