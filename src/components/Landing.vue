dd189bf902e265caac592ea783dfff40f7c9fcb7
Timo Volk <timo.volk@gmail.com>
2018-06-20 22:09:00 +0200

    added Text to Landing page

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,53 +1,71 @@
 <template>
-    <v-container fill-height grid-list-md text-xs-center>
-        <v-layout align-center>
-            <v-flex align-center>
+    <v-container>
+        <v-layout align-center row wrap>
+            <v-flex align-center xs12 text-xs-center id="header">
                 <img src='../assets/Logo_neu_icon.png'/>
-                <h3 class="display-3">Enviroomate</h3>
-                <v-alert :value='resetTokenSendAlert' type="info" >Wir haben dir eine E-Mail gesendet</v-alert>
-                <v-form ref="form" v-model="valid" lazy-validation>
-                    <v-text-field
-                            v-model="email"
-                            :rules="emailRules"
-                            label="E-Mail"
-                            required
-                    ></v-text-field>
-                    <v-text-field
-                            v-model="password"
-                            :rules="passwordRules"
-                            type="password"
-                            label="Passwort"
-                            required
-                    ></v-text-field>
-                    <v-alert :value='loginStatus == "failed"' type="error" transition="scale-transition"
-                    >
-                        Benutzername oder Passwort falsch!
-                    </v-alert>
-                    <div>
-                        <v-btn class="landingBtn" round depressed large color="primary"
-                               :disabled="!valid"
-                               @click="login()"
+                <h4 class="display-1">Willkommen bei</h4>
+                <h2 class="display-3">Enviroomate</h2>
+            </v-flex>
+            <v-flex xs12 sm6 mx-2 id="copy-text">
+                <div>Wir von Enviroommate haben uns überlegt, wie wir etwas für unser Klima tun können und besonders
+                    wichtig war uns dabei, dass wir am Ende auch Bock haben die Tipps umzusetzen. Deshalb findet ihr auf
+                    unserer Seite jede Woche eine neue Challenge, die ihr zusammen mit eurer WG erledigen könnt. Dabei
+                    gibt es immer eine Aufgabe für die von euch, die einfach gerne einmal in der Woche etwas neues,
+                    lustiges mit ihren lieben Mitbewohnern unternehmen wollen und dann noch einen Zusatztipp für die
+                    Profis unter euch. Für jede geschaffte Challenge bekommt ihr Punkte, die euren WG Baum wachsen
+                    lassen und die ihr mit befreundeten WGs vergleichen könnt. Falls ihr coole Ideen für neue Challenges
+                    habt oder euch anders einbringen wollt, schreibt uns gerne eine Mail! Wir freuen uns über alle neuen
+                    Enviroommates 💚
+                </div>
+            </v-flex>
+            <v-flex xs12 sm5 align-center text-xs-center mx-2 id="login">
+                <v-card>
+                    <v-card-text>
+                    <v-alert :value='resetTokenSendAlert' type="info">Wir haben dir eine E-Mail gesendet</v-alert>
+                    <v-form ref="form" v-model="valid" lazy-validation>
+                        <v-text-field
+                                v-model="email"
+                                :rules="emailRules"
+                                label="E-Mail"
+                                required
+                        ></v-text-field>
+                        <v-text-field
+                                v-model="password"
+                                :rules="passwordRules"
+                                type="password"
+                                label="Passwort"
+                                required
+                        ></v-text-field>
+                        <v-alert :value='loginStatus == "failed"' type="error" transition="scale-transition"
                         >
-                            Login
-                        </v-btn>
+                            Benutzername oder Passwort falsch!
+                        </v-alert>
+                        <div>
+                            <v-btn class="landingBtn" round depressed large color="primary"
+                                   :disabled="!valid"
+                                   @click="login()"
+                            >
+                                Login
+                            </v-btn>
+                        </div>
+                        <div>
+                            <v-btn small outline depressed color="accent"
+                                   @click.stop="pwResetDialog = true"
+                            >
+                                Passwort vergessen...?
+                            </v-btn>
+                        </div>
+                    </v-form>
+
+                    <v-divider class="my-3"></v-divider>
+                    <div>
+                        <v-btn depressed class="landingBtn" color="info" to="/register">Registrieren</v-btn>
                     </div>
                     <div>
-                        <v-btn small outline depressed color="accent"
-                               @click.stop="pwResetDialog = true"
-                        >
-                            Passwort vergessen...?
-                        </v-btn>
+                        <v-btn depressed class="landingBtn" color="accent" to="/hilfe">Hilfe</v-btn>
                     </div>
-                </v-form>
-
-				<v-divider class="my-3"></v-divider>
-				<div>
-					<v-btn depressed class="landingBtn" color="info" to="/register">Registrieren</v-btn>
-				</div>
-				<div>
-					<v-btn depressed class="landingBtn" color="accent" to="/hilfe">Hilfe</v-btn>
-				</div>
-
+                    </v-card-text>
+                </v-card>
             </v-flex>
         </v-layout>
 
@@ -78,7 +96,8 @@
                             <v-btn round depressed large color="primary"
                                    :disabled="!valid"
                                    @click="resetPassword"
-                            >Passwort zurücksetzen</v-btn>
+                            >Passwort zurücksetzen
+                            </v-btn>
                         </v-form>
                     </v-flex>
                 </v-layout>
@@ -90,6 +109,7 @@
 <script>
     import {mapActions, mapGetters} from 'vuex'
     import Api from '../api/api'
+
     export default {
         computed: mapGetters({
             token: 'token',
@@ -148,5 +168,11 @@
 </script>
 
 <style scoped>
+    h4 {
+        font-family: "Summer Font";
+    }
 
+    h2 {
+        font-family: "Summer Font";
+    }
 </style>
